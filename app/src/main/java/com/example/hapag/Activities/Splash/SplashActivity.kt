10dbf488bb6f13caee85cfdd394504744a8f31ff
newtical <dominicjudeseguin@gmail.com>
2025-04-13 18:52:23 +0800

    test

---
+++ app/src/main/java/com/example/hapag/Activities/Splash/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.hapag.Activities.Splash
+
+import android.os.Bundle
+import android.window.SplashScreen
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.hapag.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+@Preview
+
+fun SplashScreen() {
